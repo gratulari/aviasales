7c71c91e447d0f60e95fdfe9846dcb3d2cfa1449
gratulari <57764556+gratulari@users.noreply.github.com>
2020-04-05 02:30:25 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,14 +12,15 @@ const formSearch = document.querySelector('.form-search'),
 const citiesApi = 'database/cities.json',
     proxy = 'https://cors-anywhere.herokuapp.com/',
     API_KEY = '853c139c883e1864a947c2d64131e004',
-    calendar = 'https://min-prices.aviasales.ru/calendar_preload';
+    calendar = 'http://min-prices.aviasales.ru/calendar_preload',
+    MAX_COUNT = 10;
 
 let city = [];
 
 
 // Функции
 
-const getData = (url, callback) => {
+const getData = (url, callback, reject = console.error) => {
     const request = new XMLHttpRequest();
 
     request.open('GET', url);
@@ -30,7 +31,7 @@ const getData = (url, callback) => {
         if (request.status === 200) {
             callback(request.response);
         } else {
-            console.error(request.status);
+            reject(request.status);
         }
     });
 
@@ -88,6 +89,26 @@ const getChanges = (num) => {
     }
 };
 
+const getLinkAviasales = (data) => {
+    let link = 'https://www.aviasales.ru/search/';
+
+    link += data.origin;
+
+    const date = new Date(data.depart_date);
+
+    const day = date.getDate();
+
+    link+= day < 10 ? '0' + day : day;
+
+    const month = date.getMonth() + 1;
+
+    link += month < 10 ? '0' + month : month;
+
+    link += data.destination;
+
+    return link + '1';
+}
+
 const createCard = (data) => {
     const ticket = document.createElement('article');
     ticket.classList.add('ticket');
@@ -99,7 +120,7 @@ const createCard = (data) => {
             <h3 class="agent">${data.gate}</h3>
             <div class="ticket__wrapper">
                 <div class="left-side">
-                    <a href="https://www.aviasales.ru/search/SVX2905KGD1" class="button button__buy">Купить за ${data.value}₽</a>
+                    <a href="${getLinkAviasales(data)}" target="_blank" class="button button__buy">Купить за ${data.value}₽</a>
                 </div>
                 <div class="right-side">
                     <div class="block-left">
@@ -141,6 +162,11 @@ const renderCheapYear = (cheapTickets) => {
     otherCheapTickets.innerHTML = '<h2>Самые дешевые билеты на другие даты</h2>';
 
     cheapTickets.sort((a, b) => a.value - b.value);
+
+    for (let i = 0; i < cheapTickets.length && i < MAX_COUNT; i++) {
+        const ticket = createCard(cheapTickets[i]);
+        otherCheapTickets.append(ticket);
+    }
 
     console.log(cheapTickets);
 }
@@ -200,6 +226,9 @@ formSearch.addEventListener('submit', (event) => {
 
         getData(calendar + requestData, (data) => {
             renderCheap(data, formData.when);
+        }, (error) => {
+            alert('В этом направлении нет рейсов');
+            console.error('Ошибка', error)
         });
     } else {
         alert('Введите корректное название города');
